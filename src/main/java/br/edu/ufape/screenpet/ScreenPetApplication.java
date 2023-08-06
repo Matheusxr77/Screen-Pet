6bd06078d34b2ad210fcaa79270c553ef867cc20
Matheus Marcos Joel da Silva <70113618+Matheusxr77@users.noreply.github.com>
2023-08-06 11:17:47 -0300

    Commit Inicial

---
+++ src/main/java/br/edu/ufape/screenpet/ScreenPetApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.screenpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenPetApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/edu/ufape/screenpet/business/basic/Pet.java
@@ -0,0 +1,10 @@
+package br.edu.ufape.screenpet.business.basic;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pet {
+	@Id
+	private String id;
+}
