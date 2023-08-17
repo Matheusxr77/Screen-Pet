e68e30fb3f39a77f33b1b93a004c2be74a7acc10
Matheus Marcos Joel da Silva <70113618+Matheusxr77@users.noreply.github.com>
2023-08-17 17:27:33 -0300

    Update VaccinationHistoric.java

--- src/main/java/br/edu/ufape/screenpet/business/basic/VaccinationHistoric.java
+++ src/main/java/br/edu/ufape/screenpet/business/basic/VaccinationHistoric.java
@@ -12,26 +12,8 @@ public class VaccinationHistoric {
 	private int id;
 	private String comments;
 	
-<<<<<<< HEAD
-	public VaccinationHistoric (int id, String comments) {
-=======
 	public VaccinationHistoric() {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 541c357cf4cace52b1ff186d8ae82deda3c97944
-		
-=======
->>>>>>> 50d23a182b5c8d83f3fd31993b7ce3b42ddbfcdb
 
-	}
-
-	public String getComments() {
-		return comments;
-	}
-
-	public void setComments(String comments) {
-		this.comments = comments;
 	}
 	
 	public String getComments() {
