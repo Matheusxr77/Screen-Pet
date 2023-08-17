0df0a6b2390dfcf71fb4fda18d551c887c3e253a
Jeffersonalanmm <jefferson.alan.jm@gmail.com>
2023-08-17 19:45:22 -0300

    InterfaceColecaoAdministrator created

---
+++ src/main/java/br/edu/ufape/screenpet/data/InterfaceColecaoAdministrator.java
@@ -0,0 +1,11 @@
+package br.edu.ufape.screenpet.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.edu.ufape.screenpet.business.basic.Administrator;
+
+@Repository
+public interface InterfaceColecaoAdministrator extends JpaRepository<Administrator, Long>{
+
+}
