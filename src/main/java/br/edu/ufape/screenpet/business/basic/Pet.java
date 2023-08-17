74ce3e896040a7b4dcd7b6daea1a1abd402f8513
mayeufraferreira <mayeufraferreira@gmail.com>
2023-08-17 00:12:22 -0300

    atualização

--- src/main/java/br/edu/ufape/screenpet/business/basic/Pet.java
+++ src/main/java/br/edu/ufape/screenpet/business/basic/Pet.java
@@ -1,10 +1,48 @@
 package br.edu.ufape.screenpet.business.basic;
 
+import java.sql.Date;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class Pet {
 	@Id
-	private String id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String gender;
+	private Date dateBirth;
+	
+	public Pet(long id, String name, String gender, Date dateBirth) {
+		this.id = id;
+		this.setName(name);
+		this.setGender(gender);
+		this.setDateBirth(dateBirth);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDateBirth() {
+		return dateBirth;
+	}
+
+	public void setDateBirth(Date dateBirth) {
+		this.dateBirth = dateBirth;
+	}
 }
