e25eae997de7d837632f5fd0675f0ff8300d143f
Jeffersonalanmm <jefferson.alan.jm@gmail.com>
2023-08-17 19:53:03 -0300

    InterfaceColecaoAppointment created

--- src/main/java/br/edu/ufape/screenpet/data/InterfaceColecaoAppointment.java
+++ src/main/java/br/edu/ufape/screenpet/data/InterfaceColecaoAppointment.java
@@ -3,9 +3,9 @@ package br.edu.ufape.screenpet.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.edu.ufape.screenpet.business.basic.Schedule;
+import br.edu.ufape.screenpet.business.basic.Appointment;
 
 @Repository
-public interface InterfaceColecaoSchedule extends JpaRepository<Schedule, Long>{
+public interface InterfaceColecaoAppointment extends JpaRepository<Appointment, Long>{
 
 }
