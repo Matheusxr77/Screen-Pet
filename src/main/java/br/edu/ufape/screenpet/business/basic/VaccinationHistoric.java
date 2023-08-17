a682d8c2a23dc0e7dcfe5304fc8f41ee85a86f3f
Jeffersonalanmm <jefferson.alan.jm@gmail.com>
2023-08-17 17:26:43 -0300

    Class Administrator created

--- src/main/java/br/edu/ufape/screenpet/business/basic/VaccinationHistoric.java
+++ src/main/java/br/edu/ufape/screenpet/business/basic/VaccinationHistoric.java
@@ -11,15 +11,8 @@ public class VaccinationHistoric {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String comments;
-	
-<<<<<<< HEAD
+
 	public VaccinationHistoric (int id, String comments) {
-=======
-	public VaccinationHistoric() {
-<<<<<<< HEAD
->>>>>>> 541c357cf4cace52b1ff186d8ae82deda3c97944
-		
-=======
 
 	}
 
@@ -29,22 +22,6 @@ public class VaccinationHistoric {
 
 	public void setComments(String comments) {
 		this.comments = comments;
->>>>>>> 2c98b0a8d203c7cec70b651538985f2694b98efa
 	}
 
-	public String getComments() {
-		return comments;
-	}
-
-	public void setComments(String comments) {
-		this.comments = comments;
-	}
-	
-	public String getComments() {
-		return comments;
-	}
-	
-	public void setcomments(String comments) {
-		this.comments = comments;
-	}
 }
