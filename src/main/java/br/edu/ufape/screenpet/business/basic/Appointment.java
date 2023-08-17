bf41c686f01d3d4ca1e074e8e9fa96e924e1e35e
Matheusxr77 <suehtamxr77@gmail.com>
2023-08-17 16:05:32 -0300

    Schedule and Appointment

---
+++ src/main/java/br/edu/ufape/screenpet/business/basic/Appointment.java
@@ -0,0 +1,29 @@
+package br.edu.ufape.screenpet.business.basic;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Appointment {
+    @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+    private String anamnesis;
+
+    public Appointment(long id, String anamnesis) {
+        super();
+        this.id = id;
+        this.setAnamnesis(anamnesis);
+    }
+
+    public String getAnamnesis() {
+        return anamnesis;
+    }
+
+    public void setAnamnesis(String anamnesis) {
+        this.anamnesis = anamnesis;
+    }
+}
+
