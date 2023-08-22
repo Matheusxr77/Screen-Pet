2dc3bc2b78bbe051e71e24682bc95b02b0d1bdf7
mayeufraferreira <mayeufraferreira@gmail.com>
2023-08-21 23:52:44 -0300

    Cadastro de Tutor

---
+++ src/main/java/br/edu/ufape/screenpet/business/register/InterfaceRegisterTutor.java
@@ -0,0 +1,17 @@
+package br.edu.ufape.screenpet.business.register;
+
+import java.util.List;
+
+import br.edu.ufape.screenpet.business.basic.Tutor;
+import br.edu.ufape.screenpet.business.register.exception.DuplicateTutorException;
+import br.edu.ufape.screenpet.business.register.exception.TutorDoesNotExistException;
+
+public interface InterfaceRegisterTutor {
+	Tutor findTutorCpf(String cpf) throws TutorDoesNotExistException;
+	Tutor saveTutor(Tutor entity) throws DuplicateTutorException;
+	Tutor updateTutor(Tutor entity) throws TutorDoesNotExistException;
+	List<Tutor> listTutor();
+	boolean checkUserExistence(Long id);
+	Tutor findTutorId(Long id);
+	void deactivateTutor(String cpf) throws TutorDoesNotExistException;
+}
